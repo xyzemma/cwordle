444cee6d2a00f7140fc0de65aab7cdd61988d60f
emma <anarchoraccoon@protonmail.com>
2025-07-21 19:14:34 +0200

    Init 2

--- main.c
+++ main.c
@@ -1,40 +1,30 @@
 #include <stdio.h>
-#include <time.h>
+#include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
+char wordlist[10][6];
 
-char *getword(FILE *wordlist,int linecount) {
-    int r = rand() % linecount;
-    char ch;
-    int line_i;
-    while(!feof(wordlist)&&line_i<linecount) {
-        ch = fgetc(wordlist);
-        if(ch=='\n') {
-            line_i++;
-        }
-    }
-    
+char *chooseword() {
+    int r = rand() % 10;
+    char *word = malloc(6);
+    word = wordlist[r];
+    return word;
 }
 
-int countlines(FILE *wordlist) {
-    int lines = 0;
-    char ch;
-    while(!feof(wordlist)) {
-        ch = fgetc(wordlist);
-        if(ch == '\n') {
-            lines++;
-        }
-    }
-    fseek(wordlist,0,SEEK_SET);
-    return lines;
-}
-int moveto(int n, FILE *file) {
-    int 
-}
 int main() {
     srand(time(NULL));
-    FILE *wordlist = fopen("wordlist.txt","r");
-    int linecount = countlines(wordlist);
-    getword(wordlist,linecount);
+    strcpy(wordlist[0], "HOUSE");
+    strcpy(wordlist[1], "KINGS");
+    strcpy(wordlist[2],"PANDA");
+    strcpy(wordlist[3],"CHINA");
+    strcpy(wordlist[4],"BREAD");
+    strcpy(wordlist[5],"HEATS");
+    strcpy(wordlist[6],"FLOOR");
+    strcpy(wordlist[7],"BEANS");
+    strcpy(wordlist[8],"FLOUR");
+    strcpy(wordlist[9],"EAGLE");
+    printf("%s",chooseword());
     return 0;
 }
+
